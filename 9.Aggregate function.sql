a8747a14cf8054a4b4191f3728c6fbede3c841f0
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-08-24 14:34:48 +0530

    Create 9.Aggregate function.sql

---
+++ 9.Aggregate function.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE kamlakant;
+
+USE kamlakant;
+
+CREATE TABLE user(
+id INT,
+age INT,
+name VARCHAR(30),
+email VARCHAR(50),
+followers INT DEFAULT 0,
+following INT ,
+CONSTRAINT CHECK (age>=13),
+PRIMARY KEY(id)
+);
+
+INSERT INTO user
+(id,age,name,email,followers,following)
+VALUES
+(1,18,'adam','adam@yahoo.in',200,200),
+(2,19,'bob','bob@gmail.com',300,306),
+(3,16,'casey','casey@gmail.com',200,105);
+
+INSERT INTO user
+(id,age,name,email,followers,following)
+VALUES
+(4,10,'RAHUL','RAHUL15@GMAIL.com',400,150);
+
+SELECT avg(age)
+FROM user;
+
+CREATE TABLE post(
+id INT PRIMARY KEY,
+content VARCHAR(100),
+user_id INT,
+FOREIGN KEY (user_id) REFERENCES user(id)
+);
